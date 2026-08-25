f056af04c2a2c34b199a33d0b9225e8537fe71d2
Aryan-Protein-Vala <aryansharma24112003@gmail.com>
2026-08-25 15:56:33 +0530

    Update robot to trigger random expressions strictly on hover

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -597,19 +597,19 @@ function Robot() {
   const [expr, setExpr] = useState('normal')
 
   useEffect(() => {
-    const exprInterval = setInterval(() => {
-      // 70% chance to just stay normal/blink quickly, 30% to show a random expression for a bit longer
-      if (Math.random() > 0.3) {
-        setExpr('blink')
-        setTimeout(() => setExpr('normal'), 150)
-      } else {
-        const expressions = ['surprised', 'squint', 'joy']
-        setExpr(expressions[Math.floor(Math.random() * expressions.length)])
-        setTimeout(() => setExpr('normal'), 1200)
-      }
+    const blinkInterval = setInterval(() => {
+      setExpr(prev => (prev === 'normal' ? 'blink' : prev))
+      setTimeout(() => {
+        setExpr(prev => (prev === 'blink' ? 'normal' : prev))
+      }, 150)
     }, 4000)
-    return () => clearInterval(exprInterval)
+    return () => clearInterval(blinkInterval)
   }, [])
+
+  const handleHover = () => {
+    const expressions = ['blush', 'surprised', 'squint', 'joy']
+    setExpr(expressions[Math.floor(Math.random() * expressions.length)])
+  }
 
   return (
     <div 
@@ -624,7 +624,7 @@ function Robot() {
       </div>
       <button 
         className={`robot ${expr}`}
-        onMouseEnter={() => setExpr('blush')}
+        onMouseEnter={handleHover}
         onMouseLeave={() => setExpr('normal')}
         onClick={() => setOpen(!open)} 
         onPointerDown={(e) => {
